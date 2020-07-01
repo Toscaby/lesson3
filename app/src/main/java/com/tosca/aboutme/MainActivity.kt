9c13278b304c88594cdc02dc632547096870e736
liz <lizbyu@gmail.com>
2020-07-01 22:20:11 +0800

    feat: add data binding

--- app/src/main/java/com/tosca/aboutme/MainActivity.kt
+++ app/src/main/java/com/tosca/aboutme/MainActivity.kt
@@ -1,12 +1,37 @@
 package com.tosca.aboutme
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.tosca.aboutme.databinding.ActivityMain2Binding
 
 class MainActivity : AppCompatActivity() {
+  private lateinit var binding: ActivityMain2Binding
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main2)
+//    setContentView(R.layout.activity_main2)
+
+    binding = DataBindingUtil.setContentView(this, R.layout.activity_main2)
+  }
+
+  fun addNickname(view: View) {
+//    val editText = findViewById<EditText>(R.id.nickname_edit)
+//    val nicknameText = findViewById<TextView>(R.id.nickname_text)
+
+    binding.apply {
+      nicknameText.text = binding.nicknameEdit.text
+      nicknameEdit.visibility = View.GONE
+      view.visibility = View.GONE
+      nicknameText.visibility = View.VISIBLE
+      invalidateAll()
+    }
+
+
+    val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.hideSoftInputFromWindow(view.windowToken, 0)
   }
 }
